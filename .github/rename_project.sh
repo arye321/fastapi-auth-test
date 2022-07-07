2c628fa3242afae5cfdcca51a795328dc0388c5a
arye321 <arye321@users.noreply.github.com>
2022-07-07 20:35:42 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="arye321"
+original_name="fastapi_auth_test"
+original_urlname="fastapi-auth-test"
+original_description="Awesome fastapi_auth_test created by arye321"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi_auth_test $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
